7a7986e911e771293bc49abfc19f0674b51e04eb
samedamci <samedamci@gmail.com>
2019-04-14 10:02:18 +0200

    Create config folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const config = require('./config.json');
+const config = require('./config/config.json');
 const { prefix } = config;
 
 bot.login(config.token);
