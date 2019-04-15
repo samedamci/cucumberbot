66e7c0c933ef9a183e6b957fcea12bb07538cf9c
samedamci <samedamci@gmail.com>
2019-04-15 20:38:21 +0200

    Add welcome of new members in general channel

--- auto/members.js
+++ auto/members.js
@@ -1,5 +1,23 @@
-bot.on("guildMemberAdd", member => {
-    const channel = member.guild.channels.find(ch => ch.name === "member-log");
-    if (!channel) return;
-    channel.send(`Hello ${member}, welcome on our server!`);
+bot.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'general');
+    let memberavatar = member.user.avatarURL;
+        if (!channel) return;
+        let embed = new Discord.RichEmbed()
+        .setDescription(`
+        :wave: Hello ${member}! 
+        
+        :smile: Welcome in our server!
+        `)
+        .setThumbnail(memberavatar)
+        .setColor('GREEN')
+        channel.sendEmbed(embed);
 });
+
+bot.on('guildMemberRemove', member => {
+    let channel = member.guild.channels.find('name', 'general');
+    let memberavatar = member.user.avatarURL;
+        let embed = new Discord.RichEmbed()
+        .setDescription(`:tired_face: Oh, user ${member.user.tag} left our server...`)
+        .setColor('RED')
+        channel.sendEmbed(embed);
+})
