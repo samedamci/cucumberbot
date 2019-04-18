533948d0799129ce648701ad4267594c5444e5a1
samedamci <samedamci@gmail.com>
2019-04-18 13:44:56 +0200

    Add warning colors for ping command

--- commands/info.js
+++ commands/info.js
@@ -30,17 +30,18 @@ bot.on("message", message => {
         message.channel.send(botinfoEmbed)
     };
     if (message.content.startsWith(prefix + 'ping')) {
+        let ping = Date.now() - message.createdTimestamp;
         let ramusage = Math.floor(process.memoryUsage().rss / 1000000)
-        message.channel.send({embed: {
-            color: 0x2ed32e,
-            fields: [{
-                name: ":ping_pong: Pong",
-                value: `
-                Ping: **${Date.now() - message.createdTimestamp}** ms
-                RAM: ${ramusage} mb
-                `
-                }],
-            }
-        })
+        let color = 0x2ed32e;
+        if (ping > 90) color = 15322368; 
+        if (ping > 190) color = 16725020; 
+        let embed = new Discord.RichEmbed()
+        .setTitle(`:ping_pong: Pong!`)
+        .setDescription(`
+        Ping: **${ping}** ms
+        RAM: ${ramusage} mb
+        `)
+        .setColor(color)
+        message.channel.send(embed);
     };
 });
