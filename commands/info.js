bbdc63ac103f4af183cc13fe696e0bfeeb3c020d
samedamci <samedamci@gmail.com>
2019-04-18 15:25:13 +0200

    Edit botinfo command

--- commands/info.js
+++ commands/info.js
@@ -13,25 +13,29 @@ bot.on("message", message => {
         message.channel.send(serverinfoEmbed);
     };
     if (message.content === `${prefix}botinfo`) {
-        const botinfoEmbed = new Discord.RichEmbed()
-        .setTitle(`Cucumber Bot`)
-        .setDescription(`
-        **Language:** JavaScript (Node.JS)
-        **Library:** discord.js
-        **Versions:**
-        bot: ${botversion}
-        library: ${discordJS.version}
-        node: 11.0.0
-
-        **Online from:** ${firstdate} - ${firsttime}
-        **Online on:** ${bot.guilds.size} servers
-        `)
-        .setFooter(`Joined at: ${guild.joinedAt}`)
-        message.channel.send(botinfoEmbed)
+        let ping = Date.now() - message.createdTimestamp;
+        let ramusage = Math.floor(process.memoryUsage().rss / 1000000);
+        const embed = new Discord.RichEmbed()
+        .setAuthor(name=`Cucumber Bot`, icon="https://cdn.discordapp.com/avatars/565794682836090880/7e73680fc78223119c4d551d8a428525.png?size=2048")
+        .addField(`>> Ping`, `${ping}`, true)
+        .addField(`>> Ram usage`, `${ramusage}`, true)
+        .addField(`>> Hosted on`, `Heroku`, true)
+        .addField(`>> Online from`, `${firstdate} - ${firsttime}`, true)
+        .addField(`>> Language`, `javascript`, true)
+        .addField(`>> Environment`, `node.js [11.0.0]`, true)
+        .addField(`>> Library`, `discord.js [11.4.2]`, true)
+        .addField(`>> OS`, `Linux`, true)
+        .addField(`>> Total servers`, `${bot.guilds.size}`, true)
+        .addField(`>> Commands`, `7`, true)
+        .addField(`>> Prefix`, `${prefix}`, true)
+        .addField(`>> Author`, `samedamci#5384`, true)
+        .addField(`>> On this server from`, `${guild.joinedAt}`)
+        .addField(`>> Links`, `[webpage](https://cucumber.netlify.com)`)
+        message.channel.send(embed)
     };
     if (message.content.startsWith(prefix + 'ping')) {
         let ping = Date.now() - message.createdTimestamp;
-        let ramusage = Math.floor(process.memoryUsage().rss / 1000000)
+        let ramusage = Math.floor(process.memoryUsage().rss / 1000000);
         let color = 0x2ed32e;
         if (ping > 90) color = 15322368; 
         if (ping > 190) color = 16725020; 
