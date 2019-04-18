55839d855c3a0bc29f8980b201d483733a90ce27
samedamci <samedamci@gmail.com>
2019-04-18 21:02:04 +0200

    Add value of RAM usage in percents

--- commands/info.js
+++ commands/info.js
@@ -17,8 +17,8 @@ bot.on("message", message => {
         let ramusage = Math.floor(process.memoryUsage().rss / 1000000);
         const embed = new Discord.RichEmbed()
         .setAuthor(name=`Cucumber Bot`, icon="https://cdn.discordapp.com/avatars/565794682836090880/7e73680fc78223119c4d551d8a428525.png?size=2048")
-        .addField(`>> Ping`, `${ping}`, true)
-        .addField(`>> Ram usage`, `${ramusage}/512 mb`, true)
+        .addField(`>> Ping`, `msg: ${ping}, bot: ${bot.ping}`, true)
+        .addField(`>> Ram usage`, `${ramusage}/512 MB (${Math.round(ramusage/512*100)}%)`, true)
         .addField(`>> Hosted on`, `Heroku`, true)
         .addField(`>> Online from`, `${firstdate} - ${firsttime}`, true)
         .addField(`>> Language`, `javascript`, true)
@@ -37,13 +37,14 @@ bot.on("message", message => {
         let ping = Date.now() - message.createdTimestamp;
         let ramusage = Math.floor(process.memoryUsage().rss / 1000000);
         let color = 0x2ed32e;
-        if (ping > 90) color = 15322368;
-        if (ping > 190) color = 16725020;
+        if (ping > 90 || bot.ping > 200) color = 15322368;
+        if (ping > 190 || bot.ping > 300) color = 16725020;
         let embed = new Discord.RichEmbed()
         .setTitle(`:ping_pong: Pong!`)
         .setDescription(`
-        Ping: **${ping}** ms
-        RAM: ${ramusage}/512 mb
+        Message ping: **${ping}** ms
+        Bot ping: **${bot.ping}** ms
+        RAM: ${ramusage}/512 MB (${Math.round(ramusage/512*100)}%)
         `)
         .setColor(color)
         message.channel.send(embed);
