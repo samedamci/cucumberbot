509a37796d30134bf7f3e7e244fb9bbe1826ca96
samedamci <samedamci@gmail.com>
2019-04-22 13:07:29 +0200

    Delete bot ping

--- commands/Info/botinfo.js
+++ commands/Info/botinfo.js
@@ -4,10 +4,9 @@ bot.on("message", message => {
     if (message.content === `${prefix}botinfo`) {
         let ping = Date.now() - message.createdTimestamp;
         let ramusage = Math.floor(process.memoryUsage().rss / 1000000);
-        let botping = Math.round(bot.ping)*1;
         const embed = new Discord.RichEmbed()
         .setAuthor(name=`Cucumber Bot`, icon="https://cdn.discordapp.com/avatars/565794682836090880/7e73680fc78223119c4d551d8a428525.png?size=2048")
-        .addField(`>> Ping`, `msg: ${ping}, bot: ${botping}`, true)
+        .addField(`>> Ping`, `${ping}`, true)
         .addField(`>> Ram usage`, `${ramusage}/1858 MB (${Math.round(ramusage/1858*100)}%)`, true)
         .addField(`>> Hosted on`, `Huawei P8 Lite`, true)
         .addField(`>> OS`, `Linux [Android 6.0]`, true)
@@ -27,6 +26,6 @@ bot.on("message", message => {
         .addField(`>> Author`, `samedamci#5384`, true)
         .addField(`>> On this server from`, `${guild.joinedAt}`)
         .addField(`>> Links`, `:desktop: [webpage](https://cucumberbot.netlify.com) | :keyboard: [service](https://discord.gg/7X6cvWF) | :robot: [discordbots](https://discordbots.org/bot/565794682836090880) | :cat: [github](https://github.com/samedamci/cucumberbot) | :notepad_spiral: [TODO](https://github.com/users/samedamci/projects/2) | :moneybag: [donate](https://donatebot.io/checkout/566934496231030795)`)
-        message.channel.send(embed)
+        message.channel.send(embed);
     };
 });
--- commands/Info/ping.js
+++ commands/Info/ping.js
@@ -3,7 +3,6 @@ bot.on("message", message => {
     if (message.content.startsWith(prefix + 'ping')) {
         let ping = Date.now() - message.createdTimestamp;
         let ramusage = Math.floor(process.memoryUsage().rss / 1000000);
-        let botping = Math.round(bot.ping)*1;
         let color = 0x2ed32e;
         if (ping > 90 || botping > 200) color = 15322368;
         if (ping > 190 || botping > 300) color = 16725020;
@@ -11,7 +10,6 @@ bot.on("message", message => {
         .setTitle(`:ping_pong: Pong!`)
         .setDescription(`
         Message ping: **${ping}** ms
-        Bot ping: **${botping}** ms
         RAM: ${ramusage}/1858 MB (${Math.round(ramusage/1858*100)}%)
         `)
         .setColor(color)
